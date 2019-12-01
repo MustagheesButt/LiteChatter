b73f37f67eb1c1eff530f0bb45bc8c8b6bee9408
Mustaghees Butt <renaldpirate@gmail.com>
2019-12-01 23:59:50 +0500

    v1.1 - minor fixes and recycler for chats added

--- app/src/main/java/com/example/litechatter/ChatsFragment.kt
+++ app/src/main/java/com/example/litechatter/ChatsFragment.kt
@@ -6,16 +6,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.litechatter.databinding.FragmentChatsBinding
 
 class ChatsFragment : Fragment() {
+    private lateinit var chatsRecycler: RecyclerView
+    private lateinit var chatsAdapter: RecyclerView.Adapter<*>
+    private lateinit var chatsManager: RecyclerView.LayoutManager
+
+    private val chatsDataSet: Array<String> = arrayOf("Don", "Omar", "Mr. X")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.inflate<FragmentChatsBinding>(inflater, R.layout.fragment_chats, container, false)
+
+        chatsManager = LinearLayoutManager(this.context)
+        chatsAdapter = ChatsAdapter(chatsDataSet)
+
+        chatsRecycler = binding.chatsRecyclerView.apply {
+            setHasFixedSize(true)
+
+            layoutManager = chatsManager
+
+            adapter = chatsAdapter
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+        return binding.root
+    }
+}
+
+class ChatsAdapter(private val myDataset: Array<String>): RecyclerView.Adapter<ChatsAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MyViewHolder {
+        // create a new view
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.chat_view, parent, false) as TextView
+        // set the view's size, margins, paddings and layout parameters
+
+        return MyViewHolder(textView)
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = myDataset[position]
+    }
 
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
 }
+
--- app/src/main/java/com/example/litechatter/MainActivity.kt
+++ app/src/main/java/com/example/litechatter/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.litechatter
 
-import PagerAdapter
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TableLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.litechatter.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
@@ -21,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
@@ -37,4 +40,25 @@ class MainActivity : AppCompatActivity() {
             }
         ).attach()
     }
+}
+
+class PagerAdapter: RecyclerView.Adapter<PagerAdapter.EventViewHolder>() {
+    val eventList = listOf("0", "1", "2")
+
+    class EventViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    // Layout "layout_viewpager2_cell.xml" will be defined later
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        EventViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_viewpager2_cell, parent, false))
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        (holder.view as? TextView)?.also{
+            it.text = "Page " + eventList.get(position)
+
+            val backgroundColorResId = if (position % 2 == 0) R.color.colorPrimary else R.color.colorAccent
+            it.setBackgroundColor(ContextCompat.getColor(it.context, backgroundColorResId))
+        }
+    }
+
+    override fun getItemCount() = eventList.count()
 }
